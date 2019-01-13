8ac8e4e8ae8bd17dc6caa237f2bc057c011b98b8
sense1408 <logvinov.vlad@yahoo.com>
2019-01-13 16:55:13 +0200

    PATCH route for todo updates

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 
 const {mongoose} = require('./db/mongoose');
@@ -65,6 +66,36 @@ app.delete('/todos/:id', (req, res) => {
     return res.status(200).send({doc});
   }).catch(e => res.status(400).send());
 });
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+  .then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    return res.status(200).send({todo:todo});
+
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+
+});
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
