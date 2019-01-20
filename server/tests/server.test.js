d1e60a71527c9a11bf381c95f30929c5b1e909c8
sense1408 <logvinov.vlad@yahoo.com>
2019-01-20 21:08:14 +0200

    Add tests for POST /users/login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -219,8 +219,8 @@ describe('POST /users', () => {
          expect(user).toBeTruthy();
          expect(user.password).not.toBe(password);
          done();
-       });
-     });
+       }).catch((e) => done(e));
+     })
   });
 
   it('should return validation errors if request invalid', (done) => {
@@ -250,3 +250,56 @@ describe('POST /users', () => {
   });
 
 });
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+
+      request(app)
+       .post('/users/login')
+       .send({
+         email:users[1].email,
+         password: users[1].password
+       })
+       .expect(200)
+       .expect((res) => {
+         expect(res.header['x-auth']).toBeTruthy();
+       })
+       .end((err, res) => {
+         if(err) {
+           return done(err);
+         }
+
+         User.findById(users[1]._id).then((user) => {
+           expect(user.tokens[0]).toMatchObject({
+             access: 'auth',
+             token : res.headers['x-auth']
+           });
+           done();
+         }).catch((e) => done(e));
+       });
+  });
+
+  it('should reject invalid login', (done) => {
+
+          request(app)
+           .post('/users/login')
+           .send({
+             email: 'logvinov.vlad@yahoo.com',
+             password: '1234567'
+           })
+           .expect(400)
+           .expect((res) => {
+             expect(res.header['x-auth']).toBeFalsy();
+           })
+           .end((err, res) => {
+             if(err) {
+               return done(err);
+             }
+
+             User.findById(users[1]._id).then((user) => {
+               expect(user.tokens.length).toBe(0);
+               done();
+             }).catch((e) => done(e));
+           });
+  });
+})
