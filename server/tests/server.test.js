7e7a3fcbfe0bab8f84e21493b6ac32e6dc93529b
sense1408 <logvinov.vlad@yahoo.com>
2019-02-24 21:57:22 +0200

    Set tests to DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -302,4 +302,25 @@ describe('POST /users/login', () => {
              }).catch((e) => done(e));
            });
   });
-})
+});
+
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+
+      request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((e, res) => {
+        if(e){
+        return done(e);
+      }
+
+      User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+      }).catch((e) => done(e));
+    });
+    });
+});
